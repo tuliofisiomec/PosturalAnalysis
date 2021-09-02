4f24b25b40fdbd0b5ac062926d78b865b9593456
Aidan Farhi <aidanmcfarhi@gmail.com>
2021-09-02 18:43:40 -0400

    model still thinks a cat and regular posture is rounded shoulder posture

--- MLPrediction.py
+++ MLPrediction.py
@@ -4,11 +4,11 @@ from imageai.Classification.Custom import CustomImageClassification
 if __name__ == '__main__':
     prediction = CustomImageClassification()
     prediction.setModelTypeAsResNet50()
-    prediction.setModelPath('posture/models/model_ex-001_acc-1.000000.h5')
-    prediction.setJsonPath('posture/json/model_class.json')
-    prediction.loadModel(num_objects=1)
+    prediction.setModelPath('PostureModel/models/model_ex-004_acc-0.828571.h5')
+    prediction.setJsonPath('PostureModel/json/model_class.json')
+    prediction.loadModel(num_objects=2)
 
-    predictions, probabilies = prediction.predictImage('testimage2.png', result_count=1)
+    predictions, probabilies = prediction.predictImage('TestingImages/cat.png', result_count=2)
 
     for pred, prob in zip(predictions, probabilies):
         print(pred, " : ", prob)
--- MLTraining.py
+++ MLTraining.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
 
     model_trainer = ClassificationModelTrainer()
     model_trainer.setModelTypeAsResNet50()
-    model_trainer.setDataDirectory('posture')
+    model_trainer.setDataDirectory('PostureModel')
     model_trainer.trainModel(
-        num_objects=1, 
+        num_objects=2, 
         num_experiments=10, 
         batch_size=32,
         show_network_summary=True
